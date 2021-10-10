38f219ae6734e16bf6d4539dc7a9aefcd121dad7
DarrenFritts <84247704+DarrenFritts@users.noreply.github.com>
2021-10-10 11:40:11 -0500

    video 11.6.4

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,39 +3,30 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Chris Bay
- */
 @Controller
-@RequestMapping("events")
+@RequestMapping("events") // this will be in the "/events" folder
 public class EventController {
-
-    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
+        List<String> events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
 
+    // located in the "/events" folder in a file named "create"
     @GetMapping("create")
-    public String displayCreateEventForm(Model model) {
-        model.addAttribute("title", "Create Event");
+    public String renderCreateEventForm() {
         return "events/create";
-    }
-
-    @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName) {
-        events.add(eventName);
-        return "redirect:";
     }
 
 }
